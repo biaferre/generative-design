f1d5665802fc774bff38d62655c788cd5d10394a
Beatriz Férre <93141694+biaferre@users.noreply.github.com>
2023-06-26 12:53:50 -0300

    fixed color scheme

--- peoplesmetro.js
+++ peoplesmetro.js
@@ -2,7 +2,6 @@ function setup() {
 	c = createCanvas(windowWidth, windowHeight)
 	
 	symbols = ['╣', '╝','║','╗','╔','╩','═','╬','╚','╠','╦']
-	col = ["red","yellow","green","orange","purple","blue","pink"]
 
 	startR = random(165);
   startG = random(165);
@@ -10,17 +9,19 @@ function setup() {
 	
 	background("white");
 	
-	let varScale = random(50)
+	let varScale = floor(random(50))
 
 	let space = varScale/2
 	for(x=0; x<width + 50; x+= space){
 		for(y=0; y<height+50;y+= space){
 			
-			stroke(col[floor(random(5))])
-			fill(col[floor(random(5))])
+			fill (
+			  random(startR, startR + 180),
+        random(startG, startG + 90),
+        random(startB, startB + 90)
+			)
 			
-			
-			let a = noise(11)
+			let a = random(11)
 			
 			textSize(varScale);
 			text(symbols[floor(a)], x, y)
