3893e35242008206747ed8f5018a5dc6e86bd28b
Beatriz Férre <93141694+biaferre@users.noreply.github.com>
2023-06-26 12:04:15 -0300

    basic peoples metro algorithm

--- peoplesmetro.js
+++ peoplesmetro.js
@@ -1,29 +1,35 @@
-import p5 from 'p5'
-
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	col = ["red","yellow","green","orange","purple","blue","pink"]
+	c = createCanvas(windowWidth, windowHeight)
+	
 	symbols = ['╣', '╝','║','╗','╔','╩','═','╬','╚','╠','╦']
+	col = ["red","yellow","green","orange","purple","blue","pink"]
+
 	startR = random(165);
   startG = random(165);
   startB = random(165);
 	
-	let space = 16
+	background("white");
+	
+	let varScale = random(50)
+
+	let space = varScale/2
 	for(x=0; x<width + 50; x+= space){
 		for(y=0; y<height+50;y+= space){
+			
 			stroke(col[floor(random(5))])
 			fill(col[floor(random(5))])
 			
-			// fill(
-			// random(startR, startR + 90),
-			// random(startG, startG + 90),
-			// random(startB, startB + 90)
-			// );
 			
-			let c = random(11)
+			let a = noise(11)
 			
-			textSize(32);
-			text(symbols[floor(c)], x, y)
+			textSize(varScale);
+			text(symbols[floor(a)], x, y)
 		}
 	}
+}
+
+function keyTyped() {
+  if (key === 's') {
+		saveCanvas(c, 'myCanvas', 'jpg');
+  }
 }
