272453012cee69096009d3b970bb3e3954d9c4f3
Beatriz Férre <93141694+biaferre@users.noreply.github.com>
2023-06-26 13:04:46 -0300

    added back layer

--- peoplesmetro.js
+++ peoplesmetro.js
@@ -1,36 +1,66 @@
-function setup() {
-	c = createCanvas(windowWidth, windowHeight)
-	
-	symbols = ['╣', '╝','║','╗','╔','╩','═','╬','╚','╠','╦']
-
-	startR = random(165);
-  startG = random(165);
-  startB = random(165);
-	
-	background("white");
-	
-	let varScale = floor(random(50))
-
+function generateFront(varScale) {
 	let space = varScale/2
-	for(x=0; x<width + 50; x+= space){
-		for(y=0; y<height+50;y+= space){
-			
-			fill (
-			  random(startR, startR + 180),
-        random(startG, startG + 90),
-        random(startB, startB + 90)
-			)
-			
-			let a = random(11)
-			
-			textSize(varScale);
-			text(symbols[floor(a)], x, y)
-		}
+
+startR = random(165);
+startG = random(165);
+startB = random(165);
+
+for(x=0; x<width + 50; x+= space){
+	for(y=0; y<height+50;y+= space){
+		
+		fill (
+		  random(startR, startR + 180),
+	random(startG, startG + 90),
+	random(startB, startB + 90)
+		)
+		
+		let a = random(11)
+		
+		textSize(varScale);
+		text(symbols[floor(a)], x, y)
 	}
+}
+}
+
+function generateBack(varScale) {
+let space = varScale
+
+startR = random(165);
+startG = random(165);
+startB = random(165);
+for(x=0; x<width + 50; x+= space){
+	for(y=0; y<height+50;y+= space){
+		
+		fill (
+		  random(startR, startR - 180),
+	random(startG, startG - 90),
+	random(startB, startB - 90)
+		)
+		
+		let a = random(11)
+		
+		textSize(varScale);
+		text(symbols[floor(a)], x, y)
+	}
+}
+}
+
+
+function setup() {
+c = createCanvas(windowWidth, windowHeight)
+
+symbols = ['╣', '╝','║','╗','╔','╩','═','╬','╚','╠','╦']
+
+background("white");
+
+let varScale = floor(random(100))
+
+generateBack(varScale)
+generateFront(varScale)
 }
 
 function keyTyped() {
-  if (key === 's') {
-		saveCanvas(c, 'myCanvas', 'jpg');
-  }
+if (key === 's') {
+	saveCanvas(c, 'myCanvas', 'jpg');
+}
 }
