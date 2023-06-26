dbb05bfe53e5709ed139ee2ae065c2323a802bfd
Beatriz Férre <93141694+biaferre@users.noreply.github.com>
2023-06-26 18:08:10 -0300

    playing with texttopoints

--- todo-eh-linguagem.js
+++ todo-eh-linguagem.js
@@ -1,15 +1,37 @@
 let myFont;
+let fontSize; 
+let biArray = [];
+let r=0;
+
+
 function preload() {
   myFont = loadFont('coolvetica rg.otf');
+	fontSize = 120
 }
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	textFont(myFont)
-	textSize(60)
+	textSize(fontSize)
+
+	
+	biArray = myFont.textToPoints("oi", width/2, height/2, fontSize,{
+		sampleFactor: 0.1 // default is 0.1
+	})
+
 }
 
 function draw() {
+	background(220)
+
 	
-	text("bia", mouseX, mouseY);
+	for (let i = 0; i < biArray.length; i++) {
+		push();
+		translate(biArray[i].x,biArray[i].y);
+		rotate(r);
+		r++;
+		strokeWeight(5);
+		line(-5,-5,5,5);
+		pop();
+	}
 }
