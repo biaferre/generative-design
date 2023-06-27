10a1df352551cfa1400f32f54073951e9163b108
Beatriz Férre <93141694+biaferre@users.noreply.github.com>
2023-06-27 18:23:09 -0300

    adding click interaction

--- linguagem-origem.js
+++ linguagem-origem.js
@@ -1,50 +1,82 @@
 let myFont;
-let fontSize; 
+let fontSize;
 let biArray = [];
-let r=0;
+let r = 0;
 const tolerance = 5;
+fontSize = 200
 selectedPoint = null;
 
 function preload() {
-  myFont = loadFont('coolvetica rg.otf');
-	fontSize = 200
+	myFont = loadFont('coolvetica rg.otf');
 }
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	frameRate(60)
+
+	c = createCanvas(windowWidth, windowHeight);
 	textFont(myFont)
 	textSize(fontSize)
 
-	
-	biArray = myFont.textToPoints("palavra", width/2, height/2, fontSize,{
-		sampleFactor: 0.8 // default is 0.1
-	})	
+	allWords = []
+
 }
 
 function draw() {
-	background(0)	
-	stroke(255, 255, 255);
+	background(10)
 
-	
- for (let i = 0; i < biArray.length; i++) {
-    let myPoint = biArray[i];
-    if (myPoint === selectedPoint) {
-      push();
-      translate(mouseX, mouseY);
-      strokeWeight(5);
-      line(0, 0, 0, 0);
-      pop();
-    } else {
-      push();
+	translate(-(300 + windowWidth / 3), -300)
+
+	for (let i = 0; i < allWords.length; i++) {
+		let word = allWords[i];
+		let wordArray = word.array;
+		push()
+		translate(random(0, windowWidth), random(0, windowHeight));
+		startR = random(165);
+		startG = random(165);
+		startB = random(165);
+		stroke(startR, startG, startB);
+
+		fontSize = random(100, 240)
+
+
+		for (let j = 0; j < wordArray.length; j++) {
+			let myPoint = wordArray[j];
+
+
+			push();
 			if (frameCount >= 21) {
-				myPoint.y += random(-0.08,0.08)*frameCount
-				myPoint.x += random(-0.08,0.08)*frameCount
+				myPoint.y += random(-0.1, 0.1) * (frameCount);
+				myPoint.x += random(-0.1, 0.1) * (frameCount);
 			}
 
-      translate(myPoint.x, myPoint.y);
-      strokeWeight(5);
-      line(0, 0, 0, 0);
-      pop();
-    }
-  }
+			translate(myPoint.x, myPoint.y);
+			strokeWeight(5);
+			line(0, 0, 0, 0);
+			pop();
+		}
+	}
+	pop();
+
+}
+
+class Palavra {
+	constructor() {
+		this.array = biArray = myFont.textToPoints("palavra", width / 2, height / 2, fontSize, {
+			sampleFactor: 0.4 // default is 0.1
+		})
+	}
+}
+
+function mouseClicked() {
+	newWord = new Palavra();
+	allWords.push(newWord);
+	translate(mouseX, mouseY);
+
+}
+
+
+function keyTyped() {
+	if (key === 's') {
+		saveCanvas(c, 'myCanvas', 'jpg');
+	}
 }
