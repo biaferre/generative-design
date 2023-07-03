7f04ac288ffdc12e3c7b6fedcf48cb9a2efcc71e
Beatriz Férre <93141694+biaferre@users.noreply.github.com>
2023-07-03 14:29:06 -0300

    added dark variant

--- language/languagethrutime.js
+++ language/languagethrutime.js
@@ -2,7 +2,14 @@ let angle = 0;
 
 function setup() {
 	createCanvas(600, 600);
-	background(238,238,228);	
+	let colors = [0,1];
+	backColor = random(colors);
+	if (backColor == 0) {
+		background(0);
+	}
+	else {
+		background(238,238,228);	
+	}
 }
 
 function draw() {
@@ -18,10 +25,16 @@ function draw() {
 			drawCircle(200 + alteration,400 - alteration)
 		}
 	}
+
 }
 
-function drawCircle(a,b) { // function by asd0999 at https://editor.p5js.org/asd0999/sketches/tIxlznq_8
-  stroke(0);
+function drawCircle(a,b) { // function based on asd0999 at https://editor.p5js.org/asd0999/sketches/tIxlznq_8
+	if (backColor == 0) {
+		stroke(238,238,228);
+	}
+	else {
+		stroke(0);	
+	}
 	strokeWeight(random(0,20))
   noFill();
   let x = map(cos(angle), -1, 1, a, b);
