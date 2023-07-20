5f2d714e884f37b37a0d825734edcb2a8e834fd9
Beatriz Férre <93141694+biaferre@users.noreply.github.com>
2023-07-20 10:47:13 -0300

    fixed origin issue

--- flipbook.js
+++ flipbook.js
@@ -12,7 +12,7 @@ function setup() {
 
 function draw() {
 	strokeWeight(3);
-	curve(a*-i, b*i,mouseX, mouseY, a , b, mouseX + random(0,100), mouseY + random(0,100));
+	curve(windowWidth/2*i, windowHeight/2*-i,mouseX, mouseY, windowWidth/2*-i , windowHeight/2*i, mouseX + random(0,20), mouseY + random(0,20));
 }
 
 function keyPressed() {
